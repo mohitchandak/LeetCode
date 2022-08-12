59e79d17751b716c68f299ac3ca81f956e08f511
Mohit Chandak <65466004+mohitchandak@users.noreply.github.com>
2022-08-13 01:10:04 +0530

    Time: 4 ms (55.49%), Space: 6 MB (25.28%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        
+        
+        //using stl
+        //return sqrt(x);
+        
+        
+        //Binary Search
+        int lo = 0 , hi = x, ans = -1 ;
+        while(lo <= hi){
+            long long  mid = lo + (hi-lo) / 2 ;
+            if(mid * mid <= x) ans = mid , lo = mid + 1 ;
+            else hi = mid - 1 ;
+        }
+        return ans ;
     }
 };
