4ab4fc426aac4e26d95821b7ac868738f35f83db
Mohit Chandak <65466004+mohitchandak@users.noreply.github.com>
2022-08-26 23:46:03 +0530

    Time: 3 ms (73.16%), Space: 5.9 MB (83.90%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+    bool reorderedPowerOf2(int n) {
+        string number = sortedDigits(n);
+
+        for (int i = 0; i < 30; ++i) {
+            string powerOfTwo = sortedDigits(1 << i);
+            if (number == powerOfTwo)
+                return true;
+        }
+
+        return false;
+    }
+    string sortedDigits(int n) {
+        string digits = to_string(n);
+        sort(digits.begin(), digits.end());
+        return digits;
+    }
+};
